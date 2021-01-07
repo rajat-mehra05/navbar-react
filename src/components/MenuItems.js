839337546a9f8bc54afc3256ebbf6cde85e942b1
RAJAT MEHRA <35677839+ryanjonas05@users.noreply.github.com>
2021-01-07 23:06:02 +0530

    navbar

---
+++ src/components/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Projects",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Blogs",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Contact Me",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign up",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
